f91f869f41bbd29ff402072f7eba036dadf80bf7
moqsien <moqsien@foxmail.com>
2023-12-10 21:55:15 +0800

    shell history

--- examples/ishell/ishell.go
+++ examples/ishell/ishell.go
@@ -10,20 +10,24 @@ import (
 func main() {
 	ishell := shell.NewIShell()
 	ishell.AddCommand(&cobra.Command{
-		Use: "hello",
+		Use:   "hello",
+		Short: "Say hello to you.",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("hello, how are you?")
 		},
 	})
 	ishell.AddCommand(&cobra.Command{
-		Use: "test",
+		Use:   "test",
+		Short: "An example of subcommand.",
 	})
 	ishell.AddSubCommand("test", &cobra.Command{
-		Use: "show",
+		Use:   "show",
+		Short: "Show test info.",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("show test.")
+			fmt.Println(args)
 		},
 	})
-
+	ishell.SetHistoryFilePath(".gshell_history", 300, true)
 	ishell.Start()
 }
